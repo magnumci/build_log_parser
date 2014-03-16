716c3eaa5441128ce2f8fe974dbafe6f9c644ea5
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-03-16 10:53:13 -0500

    Add spec helper and fixtures

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require "simplecov"
+SimpleCov.start
+
+$:.unshift File.expand_path("../..", __FILE__)
+
+require "build_log_parser"
+
+def fixture_path(filename = nil)
+  path = File.expand_path("../fixtures", __FILE__)
+  filename.nil? ? path : File.join(path, filename)
+end
+
+def fixture(file)
+  File.read(File.join(fixture_path, file))
+end
