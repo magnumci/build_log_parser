78c595fb73159b9565fa4ebc6c8b6a1ce85aa824
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-03-19 08:48:28 -0500

    Add shorthand methods for metrics

--- lib/build_log_parser.rb
+++ lib/build_log_parser.rb
@@ -3,4 +3,15 @@ require "build_log_parser/parser"
 require "chronic_duration"
 
 module BuildLogParser
+  def tests(str)
+    self.new(str).tests
+  end
+
+  def coverage(str)
+    self.new(str).coverage
+  end
+
+  def duration(str)
+    self.new(str).duration
+  end
 end
