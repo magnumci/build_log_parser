afa655a3ce0a4c49e572481c15f98cb0e61f2027
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-03-22 22:40:27 -0500

    Support phpunit fail test metrics

--- lib/build_log_parser/phpunit_matcher.rb
+++ lib/build_log_parser/phpunit_matcher.rb
@@ -1,12 +1,19 @@
 module BuildLogParser
   module PHPUnitMatcher
     PHPUNIT_PATTERN = /^(OK|FAIL) \(([\d]+) tests/
+    PHPUNIT_PATTERN_ERROR =  /Tests: ([\d]+), Assertions: [\d]+, Errors: ([\d]+)/
 
     def fetch_phpunit_stats(str)
       if str =~ PHPUNIT_PATTERN
         {
           count: $2.to_i,
           failures: nil,
+          pending: nil
+        }
+      elsif str =~ PHPUNIT_PATTERN_ERROR
+        {
+          count: $1.to_i,
+          failures: $2.to_i,
           pending: nil
         }
       end
